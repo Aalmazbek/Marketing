4139a38de66b9bc55224f1384b731641347e104e
Алмазбек Ишенбеков <ishenbekovalmaz@gmail.com>
2023-05-03 00:46:13 +0600

    bugfix: LatestPosts

--- src/components/LatestPosts/LatestPosts.jsx
+++ src/components/LatestPosts/LatestPosts.jsx
@@ -18,25 +18,48 @@ import { t } from 'i18next';
 
 const postsArray = [
   {
-    data: t('LatestPosts.data1'),
+    day: '15',
+    month: 'Dec',
+    year: '2022',
+    min: 8,
     title: 'Any mechanical keyboard enthusiasts in design?',
     user: 'Maddox Fletcher',
     image: 'https://s3-alpha-sig.figma.com/img/b804/50b4/b9513862fb0927a119a8be2627d91e64?Expires=1684108800&Signature=ECs32kJKMyc~M8aOosFC1vrcZss8HPMJEPu4GGAU8YqVhhmydFKbhpgbNqXvkqT~g0n3dC65e2PPRvEgUR7etYHR8wqe1JEVimMnjvxeoUlfe5WbCMSJMnuxU6PzykHlh7lnxB5wb1KrOyuF0x1wASOODeV-n7L-0l8ZOWlVfyDAvwTxfDsvP04sba10tfYeKykhGbyWJ71CABdVbU06JDPBHVZQeZnoYzagT9afhWcUlYegTeMj7Lsg~kP7FJOv6KgWUaOcOhVm0oXwS64CjUX3fgIDqJdrLR5alSfqdjL2xr9aSpaj1xEbIc71liw7w2l~orNyvgPsPeOZH3kDaQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
   },
   {
-    data: t('LatestPosts.data2'),
+    day: '4',
+    month: 'Jul',
+    year: '2021',
+    min: 8,
     title: 'How to design a product that can grow itself 10x in year:',
     user: 'Cruz Truong',
     image: 'https://s3-alpha-sig.figma.com/img/f336/22c4/08039bca0f03abd1bb2e7f4349fc721c?Expires=1684108800&Signature=ItU78EW1sQZWjfG9P950tWT7ANxoQ0kKv8q3C9OFGlcbLCLRZ~NVSESDzckI5dqzKtr~eDxZ8R4hzbCtf3XBzjksXNOKJX9~HwyRjjAOymSkmacksj7rCV65dYKIVWUC2lpI0oTBtP6Sd~en1ur7yuoGND3Sq6Stava25dbq~PKifFsPL3OqdCplULTLjoFyhEmRfXSgW3gzbTdqwLQOkwcdyejPBjij3noOPWf15rwbVsHx9326S57shk8d1HkZHti4paZPYu~JTPdHyME2jzir4yJVKHbMFJAxG~LljuOnPThxx6vMidC8tNAUK-sDjW6tK2BPyVRfCGfCtkk6Tg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
     background: true
   },
   {
-    data: t('LatestPosts.data3'),
+    day: '19',
+    month: 'Apr',
+    year: '2019',
+    min: 8,
     title: 'Any mechanical keyboard enthusiasts in design?',
     user: 'Margaret Donnelly',
     image: 'https://s3-alpha-sig.figma.com/img/02a0/0328/0a8e049b23745a2db3b4f6113223895e?Expires=1684108800&Signature=ciCXOtTBLl6ik3b8HEBjvPgkiwDEgoHYeXrP-v7ZJtXU4EpLaMVBVDB2NR9liRe4UiDd664Tr-utkeVRHc2F7FHQSQqWvkGc4mD4i37KvP-VgNCNHyFoZG7XoTihWlkP047iyllrzvpoP5ar01i5X2sKeark43NWsVM2Mu8mPRKxfXI8WfWUnr4r67-dgOMTr59k6Q4TaUAiVcpjZVZ28MurbIpNioJ8Oy8l~AmpF93YK1UY1BR7nnJA~a-W1y0mxfc6gUFZTnMqwdVa46lGSi8vhL-RRR0DB6F36K3oGL1Cb4LJKCC2CZDbNr551okWcNsehLIm-jh72g62gl85mg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
   }
 ]
+
+
+// const getMoth = (num) => {
+//   const months = [
+//     'Jan',
+//     'Jan',
+//     'Feb',
+//     '',
+//     'Jan',
+//     'Jan',
+//     'Jan',
+//     'Jan',
+//   ]
+// }
 
 
 function LatestPosts() {
@@ -90,7 +113,7 @@ function LatestPosts() {
               <SwiperSlide className={`LatestPosts__swiper-slide ${elem.background ? 'swiper-slide-background-image' : ''}`}>
                 <div className='swiper-slide-background'></div>
                 <div className='swiper-slide-header'>
-                  <div>{elem.data}</div>
+                  <div>{elem.day} {t('LatestPosts.'+elem.month)} {elem.year} • {elem.min} {t('LatestPosts.read-mins')}</div>
 
                   <h3>{elem.title}</h3>
                 </div>
