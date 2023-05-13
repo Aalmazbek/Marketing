4561dd696837603c11a1461b5b70af952c002b18
Aliev1029 <bturaruulu@gmail.com>
2023-05-13 06:00:15 +0600

    HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -19,21 +19,36 @@ import optimizing from './assets/optimizing.png';
 import results from './assets/results.png';
 import research from './assets/research.png';
 import faqs from './assets/faqs.png'
+import basic from './assets/basicIcon.png'
+import premium from './assets/premiumIcon.png'
+import standard from './assets/standardIcon.png'
+
+
+
+import LatestPosts from '../../components/LatestPosts/LatestPosts'
+import Testimonials from '../../components/Testimonials/Testimonials'
+import { useTranslation } from 'react-i18next'
+
 
 function HomePage() {
+
+  // const darkScheme = useSelector(state => state.general.darkScheme)
+
+  const { t } = useTranslation()
+
   return (
 
     <section>
       <div className={`container  first__container`}>
         <div className={`first__container-description`}>
-          <p>DIGITAL MARKETING</p>
-          <h1>Boosts Your <br /> Website Traffic</h1>
-          <p>Etiam sollicitudin, ipsum eu pulvinar rutrum, tellus <br /> ipsum laoreet sapien, quis venenatis ante odio sit <br /> amet eros. </p>
+          <p>{t("HomePage.first__container.title")}</p>
+          <h1>{t("HomePage.first__container.h1")}</h1>
+          <p>{t("HomePage.first__container.p")}</p>
           <div className={`first__container-buttons`}>
-            <button>Try for free</button>
+            <button>{t("HomePage.first__container.button-1")}</button>
             <button>
               <img src={playIcon} alt="Icon for play video" />
-              See Our Work
+              {t("HomePage.first__container.button-2")}
             </button>
           </div>
         </div>
@@ -53,38 +68,32 @@ function HomePage() {
 
       <div className={`container second__container`}>
         <div className={`second__container-about-us`}>
-          <p>ABOUT US</p>
-          <h1>Who We Are</h1>
-          <p>
-            In hac habitasse platea dictumst. Aliquam lobortis. Lorem <br />
-            ipsum dolor sit amet, consectetuer adipiscing elit. In dui <br />
-            magna, posuere eget, vestibulum et, tempor auctor, justo. <br />
-            Pellentesque habitant morbi tristique senectus et netus et <br />
-            malesuada fames ac turpis egestas.
-          </p>
+          <p>{t("HomePage.second__container.title")}</p>
+          <h1>{t("HomePage.second__container.h1")}</h1>
+          <p>{t("HomePage.second__container.p")}</p>
           <button className={`second__container-button`}>
-            learn more
+            {t("HomePage.second__container.button")}
           </button>
         </div>
         <div className={`second__container-about`}>
           <div className={`abuot__static`}>
             <div>
               <p className={`about__static-count`}>150</p>
-              <p className={`about__static-title`}>PROJECTS</p>
+              <p className={`about__static-title`}>{t("HomePage.second__container.projects")}</p>
             </div>
             <p className={`about__static-description`}>Praesent turpis. Praesent blandit laoreet nibh. <br /> Nunc nonummy metus.</p>
           </div>
           <div className={`abuot__static`}>
             <div>
               <p className={`about__static-count`}>32k</p>
-              <p className={`about__static-title`}>HAPPY CLIENTS</p>
+              <p className={`about__static-title`}>{t("HomePage.second__container.clients")}</p>
             </div>
             <p className={`about__static-description`}>Praesent blandit laoreet nibh. Suspendisse pulvinar, <br /> augue ac venenatis condimentum.</p>
           </div>
           <div className={`abuot__static`}>
             <div>
               <p className={`about__static-count`}>20</p>
-              <p className={`about__static-title`}>YEARS OF EXPERIENCE</p>
+              <p className={`about__static-title`}>{t("HomePage.second__container.experience")}</p>
             </div>
             <p className={`about__static-description`}>Praesent blandit laoreet nibh. Suspendisse pulvinar, <br /> augue ac venenatis condimentum.</p>
           </div>
@@ -93,32 +102,32 @@ function HomePage() {
 
       <div className={`container third__container`}>
         <div className={`third__container-description`}>
-          <p>Our Services</p>
-          <h1>We Provide</h1>
+          <p>{t("HomePage.third__container.title")}</p>
+          <h1>{t("HomePage.third__container.h1")}</h1>
           <p>Nunc nonummy metus. Donec elit libero, sodales nec, <br /> volutpat a, suscipit non, turpis.</p>
         </div>
         <div className={`third__container-blocks`}>
           <div className={`third__container-block first__block`}>
             <img src={SEO} alt="" />
-            <h1>SEO</h1>
+            <h1>{t("HomePage.third__container.seo")}</h1>
             <p>Nunc nonummy metus. <br /> Donec elit libero</p>
             <img src={arrow} alt="" />
           </div>
           <div className={`third__container-block second__block`}>
             <img src={email} alt="" />
-            <h1>Email Marketing</h1>
+            <h1>{t("HomePage.third__container.email")}</h1>
             <p>Nunc nonummy metus. <br /> Donec elit libero</p>
             <img src={arrow} alt="" />
           </div>
           <div className={`third__container-block third__block`}>
             <img src={search} alt="" />
-            <h1>Search Engine <br /> Oprimization </h1>
+            <h1>{t("HomePage.third__container.search")}</h1>
             <p>Nunc nonummy metus. <br /> Donec elit libero</p>
             <img src={arrow} alt="" />
           </div>
-          <div className={`third__container-block forth__block`}>
+          <div className={`third__container-block second__block`}>
             <img src={social} alt="" />
-            <h1 > Social Marketing</h1>
+            <h1>{t("HomePage.third__container.social")}</h1>
             <p>Nunc nonummy metus. <br /> Donec elit libero</p>
             <img src={arrow} alt="" />
           </div>
@@ -127,38 +136,38 @@ function HomePage() {
 
       <div className={`container forth__container`}>
         <div className={`third__container-description`}>
-          <p>Work Flow</p>
-          <h1>Working Process</h1>
+          <p>{t("HomePage.forth__container.title")}</p>
+          <h1>{t("HomePage.forth__container.h1")}</h1>
           <p>Nunc nonummy metus. Donec elit libero, sodales nec, volutpat a, <br /> suscipit non, turpis.</p>
         </div>
         <div className={`forth__container-blocks`}>
           <div className={`forth__container-block one`}>
             <img src={planning} alt="" />
-            <p>Planning</p>
+            <p>{t("HomePage.forth__container.planning")}</p>
           </div>
           <div className={`forth__container-block two`}>
             <img src={research} alt="" />
-            <p>Research</p>
+            <p>{t("HomePage.forth__container.research")}</p>
           </div>
           <div className={`forth__container-block three`}>
             <img src={optimizing} alt="" />
-            <p>Optimizing</p>
+            <p>{t("HomePage.forth__container.optimizing")}</p>
           </div>
           <div className={`forth__container-block four`}>
             <img src={results} alt="" />
-            <p>Results</p>
+            <p>{t("HomePage.forth__container.results")}</p>
           </div>
         </div>
       </div>
 
       <div className={`container our__work`}>
-        <div className={`third__container-description`}>
-          <p>Our Work</p>
-          <h1>Case Studies</h1>
+        <div className={`our__work-descriptian`}>
+          <p>{t("HomePage.our__work.title")}</p>
+          <h1>{t("HomePage.our__work.title")}</h1>
         </div>
         <div className={`our__work-blocks`}>
           <div className={`our__work-first`}>
-            <p className={`our__work-title`}>marketing</p>
+            <p className={`our__work-title`}>{t("HomePage.our__work.marketing")}</p>
             <h1 className={`our__work-description`}>General Electric</h1>
           </div>
           <div className={`our__work-second`}>
@@ -166,100 +175,135 @@ function HomePage() {
               <div className={`our__work-branding`}>
                 <div></div>
                 <div>
-                  <p>branding</p>
-                  <h1>Bank of America</h1>
+                  <p>{t("HomePage.our__work.branding")}</p>
+                  <h1>{t("HomePage.our__work.bank")}</h1>
                   <p>Quisque id odio. Nunc egestas, <br /> augue at pellentesque laoreet, <br /> felis eros vehicul</p>
-                  <button> learn more </button>
+                  <button>{t("HomePage.our__work.button")}</button>
                 </div>
               </div>
               <div className={`our__work-seo`}>
-                <p className={`our__work-title`}>SEO</p>
+                <p className={`our__work-title`}>{t("HomePage.our__work.seo")}</p>
                 <h1 className={`our__work-description`}>Louis Vuitton</h1>
               </div>
             </div>
             <div className={`our__work-secondBlock`}>
               <div className={`our__work-marketing`}>
-                <p className={`our__work-title`}>marketing</p>
+                <p className={`our__work-title`}>{t("HomePage.our__work.marketing")}</p>
                 <h1 className={`our__work-description`}>Gillette</h1>
               </div>
               <div className={`our__work-branding`}>
                 <div></div>
                 <div>
-                  <p>SOCIAL</p>
+                  <p>{t("HomePage.our__work.social")}</p>
                   <h1>Louis Vuitton</h1>
                   <p>Quisque id odio. Nunc egestas, <br /> augue at pellentesque laoreet, <br /> felis eros vehicul</p>
-                  <button> learn more </button>
+                  <button>{t("HomePage.our__work.button")}</button>
                 </div>
               </div>
             </div>
           </div>
           <div className={`our__work-third`}>
-            <p className={`our__work-title`}>WEB</p>
+            <p className={`our__work-title`}>{t("HomePage.our__work.web")}</p>
             <h1 className={`our__work-description`}>Gillette</h1>
           </div>
         </div>
-        <button>View All</button>
+        <button>{t("HomePage.our__work.button-2")}</button>
       </div>
 
       <div className={`team__container`}>
-        <div className={`team__container-description`}>
-          <p>team</p>
-          <h1>Meet Our Team</h1>
-          <p>Since wire-frame renderings are relatively <br /> simple and fast to calculate, they are often <br /> used in cases</p>
-        </div>
-        <div className={`team__container-persons`}>
-          <div className={`team__container-Pena`}>
-            <div></div>
-            <h1 className={`team__container-name`}>Eleanor Pena</h1>
-            <p className={`team__container-profession`}>UI Designer</p>
+        <div className={`container team__container-wrapper`}>
+          <div className={`team__container-description`}>
+            <p>{t("HomePage.team__container.title")}</p>
+            <h1>{t("HomePage.team__container.h1")}</h1>
+            <p>Since wire-frame renderings are relatively <br /> simple and fast to calculate, they are often <br /> used in cases</p>
           </div>
-          <div className={`team__container-Ralph`}>
-            <div></div>
-            <h1 className={`team__container-name`}>Eleanor Pena</h1>
-            <p className={`team__container-profession`}>UI Designer</p>
-          </div>
-          <div className={`team__container-Marvin`}>
-            <div></div>
-            <h1 className={`team__container-name`}>Eleanor Pena</h1>
-            <p className={`team__container-profession`}>UI Designer</p>
+          <div className={`team__container-persons`}>
+            <div className={`team__container-Pena`}>
+              <div></div>
+              <h1 className={`team__container-name`}>Eleanor Pena</h1>
+              <p className={`team__container-profession`}>{t("HomePage.team__container.designer")}</p>
+            </div>
+            <div className={`team__container-Ralph`}>
+              <div></div>
+              <h1 className={`team__container-name`}>Ralph Edwards</h1>
+              <p className={`team__container-profession`}>{t("HomePage.team__container.manager")}</p>
+            </div>
+            <div className={`team__container-Marvin`}>
+              <div></div>
+              <h1 className={`team__container-name`}>Marvin McKinney</h1>
+              <p className={`team__container-profession`}>{t("HomePage.team__container.developer")}</p>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className={`container pricing-container`}>
+      <div className={`container pricing__container`}>
         <div className={`third__container-description`}>
-          <p>Pricing</p>
-          <h1>Check Our Pricing</h1>
-          <p>Choose the perfect plan for your needs. <br /> Always flexible to grow</p>
+          <p>{t("HomePage.pricing__container.title")}</p>
+          <h1>{t("HomePage.pricing__container.h1")}</h1>
+          <p>{t("HomePage.pricing__container.choose")}</p>
         </div>
         <div class="switch">
-          <p>MONTHLY</p>
+          <p>{t("HomePage.pricing__container.monthly")}</p>
           <input type="checkbox" />
-          <p>YEARLY (save 10%)</p>
+          <p>{t("HomePage.pricing__container.yearly")}</p>
         </div>
-        <div>
+      </div>
 
+      <div className={`container blocks__container`}>
+        <div className={`blocks__container-block1`}>
+          <h1>{t("HomePage.blocks__container.basic")}</h1>
+          <a className={`blocks__container-price`}>$29</a>
+          <img src={basic} alt="" />
+          <a className={`blocks__container-description`}>Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien</a>
+          <p>{t("HomePage.blocks__container.auto")}</p>
+          <p>{t("HomePage.blocks__container.online")}</p>
+          <p>{t("HomePage.blocks__container.posting")}</p>
+          <button>{t("HomePage.blocks__container.button")}</button>
+        </div>
+        <div className={`blocks__container-block2`}>
+          <h1>{t("HomePage.blocks__container.standard")}</h1>
+          <a className={`blocks__container-price`}>$59</a>
+          <img src={standard} alt="" />
+          <a className={`blocks__container-description`}>Maecenas nec odio et ante tincidunt tempus.</a>
+          <p>{t("HomePage.blocks__container.auto")}</p>
+          <p>{t("HomePage.blocks__container.online")}</p>
+          <p>{t("HomePage.blocks__container.posting")}</p>
+          <p>{t("HomePage.blocks__container.unique")}</p>
+          <button>{t("HomePage.blocks__container.button")}</button>
+        </div>
+        <div className={`blocks__container-block3`}>
+          <h1>{t("HomePage.blocks__container.premium")}</h1>
+          <a className={`blocks__container-price`}>$99</a>
+          <img src={premium} alt="" />
+          <a className={`blocks__container-description`}>Sed lectus. Sed consequat, leo eget bibendum sodales</a>
+          <p>{t("HomePage.blocks__container.auto")}</p>
+          <p>{t("HomePage.blocks__container.online")}</p>
+          <p>{t("HomePage.blocks__container.posting")}</p>
+          <p>{t("HomePage.blocks__container.unique")}</p>
+          <p>{t("HomePage.blocks__container.design")}</p>
+          <button>{t("HomePage.blocks__container.button")}</button>
         </div>
       </div>
 
       <div className={`container FAQS__container`}>
         <div>
           <div className={`third__container-description`}>
-            <p>FAQS</p>
-            <h1>Frequently Asked <br /> Questions</h1>
+            <p>{t("HomePage.FAQS__container.title")}</p>
+            <h1>{t("HomePage.FAQS__container.h1")}</h1>
           </div>
           <div className={`FAQS__container-description`}>
             <p>Sed augue ipsum, egestas nec, vestibulum et</p>
             <h1>Curabitur nisi. Phasellus blandit leo ut odio. Donec posuere vulputate <br /> arcu. Donec mi odio, faucibus at, scelerisque quis, convallis in,</h1>
-            <hr className={`FAQS__container-hr`}/>
+            <hr className={`FAQS__container-hr`} />
             <p>Malesuada adipiscing, dui vestibulum suscipit nulla quis orci.</p>
-            <hr className={`FAQS__container-hr`}/>
+            <hr className={`FAQS__container-hr`} />
             <p>Ut varius tincidunt libero.</p>
-            <hr className={`FAQS__container-hr`}/>
+            <hr className={`FAQS__container-hr`} />
             <p>In ut quam vitae odio lacinia tincidunt.</p>
-            <hr className={`FAQS__container-hr`}/>
+            <hr className={`FAQS__container-hr`} />
             <p>Fusce vel dui Morbi nec metus.</p>
-            <hr className={`FAQS__container-hr`}/>
+            <hr className={`FAQS__container-hr`} />
           </div>
         </div>
         <div>
@@ -268,7 +312,12 @@ function HomePage() {
       </div>
       {/* <img src={ratio} alt="" className={`container section__image`} /> */}
 
+      <Testimonials />
+      <LatestPosts />
+
     </section>
+
+
   )
 }
 
